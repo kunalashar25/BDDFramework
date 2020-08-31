485af0f544a814c47d94e93d0ce497a353fce25b
kunalashar25 <kunalashar.25@gmail.com>
2020-08-31 13:20:42 +0800

    code added to capture screenshot on failure

--- src/test/java/stepDefinition/hooks/Hooks.java
+++ src/test/java/stepDefinition/hooks/Hooks.java
@@ -1,17 +1,53 @@
 package stepDefinition.hooks;
 
+import core.driverManager.BrowserType;
+import core.driverManager.DriverFactory;
 import io.cucumber.java.After;
 import io.cucumber.java.Before;
+import io.cucumber.java.Scenario;
+import io.cucumber.messages.internal.com.google.common.util.concurrent.Uninterruptibles;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
+import org.openqa.selenium.WebDriver;
+
+import java.util.concurrent.TimeUnit;
 
 public class Hooks {
 
-    @Before
-    public void setUp(){
-        System.out.println("before");
+    private WebDriver driver;
+    private String homePageUrl = "https://www.online-calculator.com/full-screen-calculator/";
+
+    /**
+     * To initiate new driver for current scenario execution
+     */
+    @Before(order = 1)
+    public void setUp() {
+        this.driver = DriverFactory.getWebDriver(BrowserType.CHROME);
+        this.driver.manage().window().maximize();
+        this.driver.get(homePageUrl);
     }
 
+    /**
+     * To close browser after all processes are completed
+     */
+    @After(order = 1)
+    public void tearDown() {
+        System.out.println("closing browser");
+        // adding sleep to view result on UI before it is closed. No need to write this line in actual project
+        Uninterruptibles.sleepUninterruptibly(2, TimeUnit.SECONDS);
+        this.driver.quit();
+    }
+
+    /**
+     * Capturing screenshot on scenario failure
+     *
+     * @param scenario current scenario
+     */
     @After
-    public void tearDown(){
-        System.out.println("after");
+    public void captureScreenshotOnFailure(Scenario scenario) {
+        if (scenario.isFailed()) {
+            byte[] screenshot = ((TakesScreenshot) driver).getScreenshotAs(OutputType.BYTES);
+            scenario.attach(screenshot, "image/png", "Execution Failed");
+        }
     }
 }
